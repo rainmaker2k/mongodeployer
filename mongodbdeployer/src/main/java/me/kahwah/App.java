27364c35a941b46080fb402b9c772b3e0fb93b1c
Kah Tang <rainmaker2k@gmail.com>
2014-06-02 23:24:56 +0200

    Trying out some system watching

--- mongodbdeployer/src/main/java/me/kahwah/App.java
+++ mongodbdeployer/src/main/java/me/kahwah/App.java
@@ -5,9 +5,13 @@ import me.kahwah.models.Component;
 import org.mongodb.morphia.query.Query;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 /**
  * Hello world!
@@ -43,5 +47,54 @@ public class App
         System.out.println("Config: " + deployer.getConfig().getString("database"));
 
         System.out.println( "Hello World!" );
+
+        try {
+            WatchService watcher = FileSystems.getDefault().newWatchService();
+
+            WatchKey key;
+
+            Path dir = FileSystems.getDefault().getPath("incoming");
+            DirectoryStream<Path> files = Files.newDirectoryStream(dir);
+            for (Path existingFile : files) {
+                System.out.println(existingFile.toString());
+            }
+            dir.register(watcher, ENTRY_CREATE);
+
+            for (;;) {
+
+                key = watcher.take();
+
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    WatchEvent.Kind<?> kind = event.kind();
+
+                    if (kind == OVERFLOW) {
+                        continue;
+                    }
+
+                    WatchEvent<Path> ev = (WatchEvent<Path>)event;
+                    Path filename = ev.context();
+
+                    Path child = dir.resolve(filename);
+                    String mimetype = Files.probeContentType(child);
+                    if (!"text/plain".equals(mimetype)) {
+                        System.err.format("New file '%s'" +
+                                " is not a plain text file.%n", filename);
+                        continue;
+                    }
+
+                    System.out.println("File placed: " + filename);
+                }
+
+                boolean valid = key.reset();
+                if (!valid) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
