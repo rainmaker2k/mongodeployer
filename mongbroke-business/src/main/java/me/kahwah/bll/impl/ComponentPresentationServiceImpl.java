c25b8635b56c6ea6b70ab379dd3443e03efaca47
Kah Tang <rainmaker2k@gmail.com>
2014-06-25 23:58:04 +0200

    Added business layer

---
+++ mongbroke-business/src/main/java/me/kahwah/bll/impl/ComponentPresentationServiceImpl.java
@@ -0,0 +1,48 @@
+package me.kahwah.bll.impl;
+
+import me.kahwah.bll.ComponentPresentationService;
+import me.kahwah.dao.ComponentPresentationDAO;
+import me.kahwah.dao.models.Component;
+import me.kahwah.dd4t.contentmodel.ComponentPresentation;
+import me.kahwah.dd4t.contentmodel.Field;
+import me.kahwah.dd4t.contentmodel.GenericComponent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by rainmaker2k on 25/06/14.
+ */
+public class ComponentPresentationServiceImpl implements ComponentPresentationService {
+
+    private ComponentPresentationDAO componentPresentationDAO;
+
+    @Override
+    public void save(ComponentPresentation componentPresentation) {
+
+        me.kahwah.dao.models.ComponentPresentation dbComponentPresentation =
+                new me.kahwah.dao.models.ComponentPresentation();
+
+        Component component = new Component();
+        Map<String, Object> componentFields = new HashMap<String, Object>();
+        component.setFields(componentFields);
+
+        GenericComponent genComponent = componentPresentation.getComponent();
+        Map<String, Field> fields = genComponent.getContent();
+
+        for (String fieldName : fields.keySet()) {
+            Field field = fields.get(fieldName);
+//            componentFields.put(fieldName, field.)
+        }
+
+        dbComponentPresentation.setComponent(component);
+    }
+
+    public ComponentPresentationDAO getComponentPresentationDAO() {
+        return componentPresentationDAO;
+    }
+
+    public void setComponentPresentationDAO(ComponentPresentationDAO componentPresentationDAO) {
+        this.componentPresentationDAO = componentPresentationDAO;
+    }
+}
--- mongodeployer/src/main/java/me/kahwah/deployer/impl/ComponentPresentationProcessorImpl.java
+++ mongodeployer/src/main/java/me/kahwah/deployer/impl/ComponentPresentationProcessorImpl.java
@@ -1,7 +1,9 @@
 package me.kahwah.deployer.impl;
 
+import me.kahwah.bll.ComponentPresentationService;
 import me.kahwah.dao.ComponentDAO;
 import me.kahwah.dao.ComponentPresentationDAO;
+import me.kahwah.dd4t.contentmodel.ComponentPresentation;
 import me.kahwah.dd4t.core.serializers.Serializer;
 import me.kahwah.deployer.PackageService;
 import me.kahwah.deployer.Processor;
@@ -11,6 +13,7 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * Created by rainmaker2k on 10-06-14.
@@ -20,14 +23,17 @@ public class ComponentPresentationProcessorImpl implements Processor<Section> {
     private static Logger log = LoggerFactory.getLogger(ComponentPresentationProcessorImpl.class);
 
     private PackageService packageService;
-    private Serializer serializer;
-    private ComponentDAO componentDao;
-    private ComponentPresentationDAO componentPresentationDao;
+    private ComponentPresentationService componentPresentationService;
 
     @Override
     public void process(Section section, String extractedDir) {
 
-        Path xmlfilePath = Paths.get(extractedDir, section.getName());
+        Path componentPresentationsFilePath = Paths.get(extractedDir, section.getName());
+        List<ComponentPresentation> componentPresentations = packageService.getSectionItems(section, section.getName());
+
+        for (ComponentPresentation componentPresentation : componentPresentations) {
+            componentPresentationService.save(componentPresentation);
+        }
         //packageService.get
 //        Component component;
 //        try {
@@ -39,30 +45,6 @@ public class ComponentPresentationProcessorImpl implements Processor<Section> {
 //        }
     }
 
-    public Serializer getSerializer() {
-        return serializer;
-    }
-
-    public void setSerializer(Serializer serializer) {
-        this.serializer = serializer;
-    }
-
-    public ComponentDAO getComponentDao() {
-        return componentDao;
-    }
-
-    public void setComponentDao(ComponentDAO componentDao) {
-        this.componentDao = componentDao;
-    }
-
-    public ComponentPresentationDAO getComponentPresentationDao() {
-        return componentPresentationDao;
-    }
-
-    public void setComponentPresentationDao(ComponentPresentationDAO componentPresentationDao) {
-        this.componentPresentationDao = componentPresentationDao;
-    }
-
     public PackageService getPackageService() {
         return packageService;
     }
@@ -70,4 +52,13 @@ public class ComponentPresentationProcessorImpl implements Processor<Section> {
     public void setPackageService(PackageService packageService) {
         this.packageService = packageService;
     }
+
+    public ComponentPresentationService getComponentPresentationService() {
+        return componentPresentationService;
+    }
+
+    public void setComponentPresentationService(ComponentPresentationService componentPresentationService) {
+        this.componentPresentationService = componentPresentationService;
+    }
+
 }
